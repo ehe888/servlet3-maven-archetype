3002f0b27ddf0843d72651f41812d3232805a068
ehe888 <eric.leihe@gmail.com>
2014-02-21 12:22:06 +0800

    Add keystore Add log.properties
    Fix some minor issues

--- src/main/resources/archetype-resources/src/main/java/HelloServlet.java
+++ src/main/resources/archetype-resources/src/main/java/HelloServlet.java
@@ -3,15 +3,17 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import javax.servlet.GenericServlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
 import java.io.IOException;
 
 @WebServlet(value="/hello", name="helloServlet")
-public class HelloServlet extends GenericServlet {
+public class HelloServlet extends HttpServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         res.getWriter().println("Hello world!!");
